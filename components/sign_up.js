d235990862f52d5c82f2d34ee556545c6920ba84
hrishi720 <hrishikeshchoudhury9954@gmail.com>
2023-03-02 23:37:08 +0530

    Initial commit

---
+++ components/sign_up.js
@@ -0,0 +1,30 @@
+let switchCtn = document.querySelector("#switch_cnt")
+let switchC1 = document.querySelector("switch_c1")
+let switchC2 = document.querySelector("#switch_c2")
+let switchCircle = document.querySelectorAll(".switch_circle")
+let switchBtn = document.querySelectorAll(".switch_btn")
+let aContainer = document.querySelector("#a-container")
+let bContainer = document.querySelector("#b-container")
+let allButtons = document.querySelectorAll(".submit")
+
+let getButtons = (e) => e.preventDefault()
+
+let changeForm = (e) => {
+    switchCtn.classList.add("is-gx")
+    setTimeout(function () {
+        switchCtn.classList.remove("is-gx")
+    }, 1500)
+    switchC1.classList.toggle("is-hidden")
+    switchC2.classList.toggle("is-hidden")
+    aContainer.classList.toggle("is-txl")
+    bContainer.classList.toggle("is-txl")
+    bContainer.classList.toggle("is-z200")
+}
+
+let mainF = (e) => {
+    for (var i = 0; i < allButtons.length; i++)
+        allButtons[i].addEventListener("click", getButtons)
+    for (var i = 0; i < switchBtn.length; i++)
+        switchBtn[i].addEventListener("click", changeForm)
+}
+window.addEventListener("load", mainF)
